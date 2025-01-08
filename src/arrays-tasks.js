df772d845d8614f10479f26058d94c897d8404b3
Natalya M <natalyamerr@gmail.com>
2025-01-09 01:51:44 +0300

    feat: getTail solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -195,8 +195,9 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  const sliceStart = arr.length - n;
+  return arr.slice(sliceStart);
 }
 
 /**
